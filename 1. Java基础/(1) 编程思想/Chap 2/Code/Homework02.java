875719c6951b5acfee8bfdf0dd67747a60902a70
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-11 15:08:37 +0800

    Create Homework02.java

---
+++ 1. Java基础/(1) 编程思想/Chap 2/Code/Homework02.java
@@ -0,0 +1,10 @@
+//将个人的基本信息（姓名、性别、籍贯、住址）打印到控制台上输出。各条信息分别占一行
+public class Homework02{
+
+  // 编写一个main方法
+  public static void main(String[] args){
+    //考察转义字符
+    System.out.println("姓名\t性别\t籍贯\t住址\t钰渤\t男\t江西\t广东");
+  }
+  
+}
