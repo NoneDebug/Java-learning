bccc2d23cf39f1bfafe892016f261966e5953da1
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-11 15:11:23 +0800

    Create Homework03.java

---
+++ 1. Java基础/(1) 编程思想/Chap 2/Code/Homework03.java
@@ -0,0 +1,13 @@
+//JDK,JRE,JVM的关系
+
+/*
+  1. JDK = JRE + java开发工具
+  2. JRE = JVM + 核心类库
+  3. JDK = JVM + 核心类库 + java开发工具
+*/
+
+public class Homework04{
+  public class void main(String[] args){
+    ...
+  }
+}
