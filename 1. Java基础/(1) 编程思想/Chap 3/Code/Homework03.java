3147d16318e96a07d2f43e64b2645fddd0f49ea0
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-12 14:24:39 +0800

    Update Homework03.java

--- 1. Java基础/(1) 编程思想/Chap 3/Code/Homework03.java
+++ 1. Java基础/(1) 编程思想/Chap 3/Code/Homework03.java
@@ -4,12 +4,12 @@ public class Homework03{
     String s1 = "红楼梦";
     String s2 = "西游记";
     String s3 = s1 + s2;
-    System.out.println(s3);
+    System.out.println(s3);  //输出红楼梦西游记
     char c1 = '男';
     char c2 = '女';
-    System.out.println(c1 + c2);
-    int p1 = 10;
-    int p2 = 15;
-    System.out.println(p1 + p2);
+    System.out.println(c1 + c2);  //输出 男对应码值 + 女对应码值之和
+    double p1 = 123.56;
+    double p2 = 100.11;
+    System.out.println(p1 + p2);  //输出223.67
   }
 }
