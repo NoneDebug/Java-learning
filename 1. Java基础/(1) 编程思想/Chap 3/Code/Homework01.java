74f3696e19b5c3474b6477353f364b94f77a72db
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-12 14:14:26 +0800

    Create Homework01.java

---
+++ 1. Java基础/(1) 编程思想/Chap 3/Code/Homework01.java
@@ -0,0 +1,15 @@
+//这是一道读程序题，预测输出
+public class Homework01 {
+  public static void main(Stringlargs){
+    int n1;    
+    n1 = 13;    //n1 赋值为 13
+    int n2;  
+    n2 = 17;    //n2 赋值为17
+    int n3;
+    n3 = n1 + n2;  //n3 赋值为n1 + n2即为30
+    System.out.println("n3 = " + n3);  //输出字符串 "n3 = 30"
+    int n4 = 38;   //n4 定义和赋值为38
+    int n5 = n4 - n3;  // n5 定义和赋值为n4 - n3（即为8）
+    System.out.println("n5 = " + n5);  //输出字符串 "n5 = 8"
+  }
+}
