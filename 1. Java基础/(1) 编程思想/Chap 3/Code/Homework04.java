d3c0caf7e8a04859458544688580411e64439017
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-12 14:31:53 +0800

    Update Homework04.java

--- 1. Java基础/(1) 编程思想/Chap 3/Code/Homework04.java
+++ 1. Java基础/(1) 编程思想/Chap 3/Code/Homework04.java
@@ -1,3 +1,23 @@
+/*
+  要求：
+    (1) 用变量名将姓名、年龄、成绩、性别、爱好存储
+    (2) 使用加号
+    (3) 使用适当的注释
+    (4) 添加转义字符，使用一条语句输出
+*/
+
 public class Homework04{
-  
+  public static void main(String[] args){
+    System.out.println("姓名" + "\t" + "年龄" + "\t" +
+                       "成绩" + "\t" + "性别" + "\t" +
+                       "爱好");
+    String name = "Yubo Tang";
+    int age = 23;
+    double grade = 567.0;
+    char gender = "男";
+    String hobby = "唱、" + "跳、" + "rap、" + "篮球";
+    System.out.println(name + "\t" + age + "\t" +
+                       grade + "\t" + gender + "\t" +
+                       hobby);
+  }
 }
