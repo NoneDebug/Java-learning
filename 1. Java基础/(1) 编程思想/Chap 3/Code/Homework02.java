f00a2794e1cc19c1a8cf9a194d9b636c3a5fd7ed
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-12 14:18:16 +0800

    Create Homework02.java

---
+++ 1. Java基础/(1) 编程思想/Chap 3/Code/Homework02.java
@@ -0,0 +1,19 @@
+//使用char类型分别保存 \n \t \r \\ 1 2 3等字符，并打印输出
+public class Homework02{
+  public static void main(String[] args){
+    char c1 = '\n';
+    char c2 = '\t';
+    char c3 = '\r';
+    char c4 = '\\';
+    char c5 = '1';
+    char c6 = '2';
+    char c7 = '3';
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println(c3);
+    System.out.println(c4);    
+    System.out.println(c5);
+    System.out.println(c6);
+    System.out.println(c7);
+  }
+}
