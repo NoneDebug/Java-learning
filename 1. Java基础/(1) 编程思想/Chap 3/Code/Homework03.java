dfe426d099d73dd111fc5854a2b5f368ec81c52c
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-12 14:22:23 +0800

    Create Homework03.java

---
+++ 1. Java基础/(1) 编程思想/Chap 3/Code/Homework03.java
@@ -0,0 +1,15 @@
+public class Homework03{
+
+  public static void main(String[] args){
+    String s1 = "红楼梦";
+    String s2 = "西游记";
+    String s3 = s1 + s2;
+    System.out.println(s3);
+    char c1 = '男';
+    char c2 = '女';
+    System.out.println(c1 + c2);
+    int p1 = 10;
+    int p2 = 15;
+    System.out.println(p1 + p2);
+  }
+}
