b50d068a631058878cedd864f3cbccb57ca40540
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2023-12-16 08:58:11 +0800

    Create Homework01.java

---
+++ 1. Java基础/(1) 编程思想/Chap 6/Code/Homework01.java
@@ -0,0 +1,11 @@
+/*
+
+1.下面数组定义正确的有
+
+A. String strs[] = {'a' ,'b', 'c'}; //error, char -> String
+B. String[] strs = {"a", "b", "c"}; //ok
+C. String[] strs = new String{"a" "b" "c"}://error
+D. String strs[] = new String[]{"a", "b", "c"};//ok
+E. String[] strs = new String[3]{"a", "b","c"};//error,不能加数字
+
+*/
