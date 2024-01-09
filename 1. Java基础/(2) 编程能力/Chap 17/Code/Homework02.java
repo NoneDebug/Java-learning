4863a303a7767259f6343a097466d86f3c11dda4
Bazinga <118868085+NoneDebug@users.noreply.github.com>
2024-01-09 10:58:52 +0800

    Update Homework02.java

--- 1. Java基础/(2) 编程能力/Chap 17/Code/Homework02.java
+++ 1. Java基础/(2) 编程能力/Chap 17/Code/Homework02.java
@@ -1,15 +1,13 @@
 package com.tybxx.Homework;
 
-/**
- * @author 唐钰渤
- * @version 1.0
- */
 public class Homework02 {
     public static void main(String[] args) {
         Card card1 = new Card();
         Card card2 = new Card();
+        Card card3 = new Card();
         card1.start();
         card2.start();
+        card3.start();
     }
 }
 
@@ -18,18 +16,23 @@ class Card extends Thread {
 
     @Override
     public synchronized void run() {
-        while(true){
+
+        while (true) {
             System.out.println("线程 = " + Thread.currentThread().getName() + " 余额 = " + credit);
-            credit -= 1000;
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                System.out.println(e.getMessage());;
+                System.out.println(e.getMessage());
+                ;
             }
-            if(credit <= 1000){
+            if (credit < 1000) {
                 System.out.println("余额不足，退出");
                 break;
             }
+            credit -= 1000;
+
         }
+        System.out.println("余额为：" + credit);
     }
 }
